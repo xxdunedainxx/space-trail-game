a7c738361fd912f2b50632f028bb52a243bf9235
xxdunedainxx <zrmmaster92@gmail.com>
2021-07-01 23:15:35 -0700

    created a door

--- space-trail-demo/Assets/scripts/core/clicking/ClickManager.cs
+++ space-trail-demo/Assets/scripts/core/clicking/ClickManager.cs
@@ -13,12 +13,14 @@ public class ClickManager : MonoBehaviour
         if(Input.GetMouseButtonDown(0))
         {
             Vector2 mousePosition = cam.ScreenToWorldPoint(Input.mousePosition);
+            
             RaycastHit2D hit = Physics2D.Raycast(mousePosition, Vector2.zero);
 
             Debug.unityLogger.Log("ClickManager is detecting if a clickable was clicked...");
 
             if(hit)
             {
+                Debug.unityLogger.Log("something hit?");
                 IClickable clickable = hit.collider.GetComponent<IClickable>();
                 clickable?.click();
             }
